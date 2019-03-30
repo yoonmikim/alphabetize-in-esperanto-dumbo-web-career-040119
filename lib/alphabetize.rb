5b0b0d2e5dfcd690c27721476bebf9abeab57acb
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-30 03:38:20 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-    arr.sort_by {|el| el.split("").map{ |char| esperanto_alphabet.index(char)} }
+    arr.sort_by {|el| el.split("").map{ |char| esperanto_alphabet.index(char)} }`
 end
